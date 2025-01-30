fc5069cd2968a7665f58f8fd49e1e77cf72cad8c
Giselle <gisellevonbingen@gmail.com>
2025-01-30 17:20:21 +0900

    Bump minecolonies version

--- src/main/java/steve_gall/minecolonies_letsdo/module/common/LetsDoV2Module.java
+++ src/main/java/steve_gall/minecolonies_letsdo/module/common/LetsDoV2Module.java
@@ -3,6 +3,7 @@ package steve_gall.minecolonies_letsdo.module.common;
 import java.util.function.Supplier;
 
 import net.minecraftforge.fml.ModList;
+import steve_gall.minecolonies_letsdo.core.common.MineColoniesLetsDo;
 
 public class LetsDoV2Module<MODULE extends AbstractModule> extends OptionalModule<MODULE>
 {
@@ -16,8 +17,20 @@ public class LetsDoV2Module<MODULE extends AbstractModule> extends OptionalModul
 	{
 		return super.canLoad() && ModList.get().getModContainerById(this.getModId()).filter(c ->
 		{
-			var majorVersion = c.getModInfo().getVersion().getMajorVersion();
-			return majorVersion == 2;
+			var rawVersion = c.getModInfo().getConfig().<String> getConfigElement("version").orElse("");
+			var index = rawVersion.indexOf('.');
+
+			try
+			{
+				var majorVersion = Integer.parseInt(rawVersion.substring(0, index));
+				return majorVersion == 2;
+			}
+			catch (Exception e)
+			{
+				MineColoniesLetsDo.LOGGER.error(e);
+				return true;
+			}
+
 		}).isPresent();
 	}
 
